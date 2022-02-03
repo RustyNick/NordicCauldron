214e3db548500b1e87d33140e498d2ce5d75ad36
Rusty-nick <rusty-nick@live.com>
2022-02-03 12:12:10 +0100

    commit files

--- client/src/components/ItemProduct.jsx
+++ client/src/components/ItemProduct.jsx
@@ -6,9 +6,6 @@ const source = "/images/"
 
 const ItemProduct = ({ item }) => (
     < div style={{ minHeight: '300px', maxWidth: "300px", overflow: 'hidden', }}>
-        {
-            console.log(item)
-        }
 
         <div
             style={{ width: '100%', height: '100%', minHeight: '260px', minWidth: '300px', backgroundColor: '#FFF', display: 'flex', justifyContent: 'center', objectFit: 'contain', }}>
